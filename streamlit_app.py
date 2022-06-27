91ff5268e4f8ac9a8c7425bc1efb8eeec566e012
eonenry <72388206+eonenry@users.noreply.github.com>
2022-06-27 05:22:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('🥣Breakfast Menu')
+streamlit.header('🥣 Breakfast Menu')
 streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑 Hard-Boiled Free-Range Egg')
